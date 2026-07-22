5896ac7e0b791d3bfce537e72f6f1d2e5110a420
devopsgit <dhananjayacg01@gmail.com>
2026-07-23 03:48:28 +0530

    Added application security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,3 +48,38 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_security_group" "app" {
+  name        = "${var.project_name}-security-group"
+  description = "Security group for application server"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project_name}-security-group"
+    Environment = var.environment
+  }
+}
